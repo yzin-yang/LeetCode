c382ebacc339788237fec9a20ffb04f0616ae277
yeung <734880084@qq.com>
2019-05-31 10:28:22 +0800

    有序two sum

--- problems/167. Two Sum II - Input array is sorted/index.js
+++ problems/167. Two Sum II - Input array is sorted/index.js
@@ -7,4 +7,19 @@
  * 双指针
  */
 var twoSum = function (numbers, target) {
+    let i = 0, j = numbers.length - 1;
+    while (i < j) {
+        let left = numbers[i];
+        let right = numbers[j];
+        if (left + right === target) {
+            return [i + 1, j + 1];
+        } else if (left + right > target) {
+            j--;
+        } else {
+            i++;
+        }
+    }
+    return [];
 };
+
+console.log(twoSum([2, 3, 7, 9], 10));
