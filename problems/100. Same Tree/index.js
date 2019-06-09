69e1959193d1f973319db322b8cd36682992b7d9
yeung <734880084@qq.com>
2019-06-09 23:26:41 +0800

    相同的树（模板字符串）

---
+++ problems/100. Same Tree/index.js
@@ -0,0 +1,45 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} p
+ * @param {TreeNode} q
+ * @return {boolean}
+ */
+//深度优先遍历
+var isSameTree = function (p, q) {
+    let tree1 = preOrder(p, "");
+    let tree2 = preOrder(q, "");
+    return tree1 === tree2;
+
+    function preOrder(root, str) {
+        str += root !== null ? root.val : null;
+        if (root !== null) {
+            str += preOrder(root.left, "");
+            str += preOrder(root.right, "");
+        }
+        return str;
+    }
+};
+
+var isSameTree = function (p, q) {
+    if (p === null && q === null)
+        return true;
+    if (p && q && p.val === q.val)
+        return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+    return false;
+};
+
+//模板字符串
+var isSameTree = function (p, q) {
+    var serialize = function (root) {
+        if (!root)
+            return "#";
+        return `${root.val}${serialize(root.left)}${serialize(root.right)}`;
+    };
+    return serialize(p) === serialize(q);
+};
