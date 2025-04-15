217096564e1f594364d7dd5a317b75bd3d8801f0
yzin <734880084@qq.com>
2025-04-15 19:05:54 +0800

    746空间复杂度优化

--- 动态规划/746. 使用最小花费爬楼梯.ts
+++ 动态规划/746. 使用最小花费爬楼梯.ts
@@ -4,12 +4,17 @@
 // dp[2] = min(dp[1] + 15, dp[0] + 10) = 10
 // dp[3] = min(dp[2] + 20, dp[1] + 15) = 15
 function minCostClimbingStairs(cost: number[]): number {
-    const dp = [0, 0];
-    if (cost.length < 2) {
+    const n = cost.length;
+    let pre1 = 0;
+    let pre2 = 0;
+    let cur = 0;
+    if (n < 2) {
         return 0;
     }
-    for (let i = 2; i <= cost.length; i++) {
-        dp[i] = Math.min(dp[i - 1] + cost[i - 1], dp[i - 2] + cost[i - 2])
+    for (let i = 2; i <= n; i++) {
+        cur = Math.min(pre1 + cost[i - 1], pre2 + cost[i - 2]);
+        pre2 = pre1;
+        pre1 = cur;
     }
-    return dp.at(-1) || 0;
+    return cur;
 };
