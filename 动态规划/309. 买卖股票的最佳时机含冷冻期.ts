db7cb4e9322eaa4351d3687257894cbae292f9ea
yzin <734880084@qq.com>
2025-05-05 23:24:13 +0800

    Update 309. 买卖股票的最佳时机含冷冻期.ts

--- 动态规划/309. 买卖股票的最佳时机含冷冻期.ts
+++ 动态规划/309. 买卖股票的最佳时机含冷冻期.ts
@@ -1,4 +1,23 @@
+// dp[i][0]：第 i 天未持有股票的最大收益
+// dp[i][1]：第 i 天持有股票的最大收益
+// dp[i][0] = max(dp[i-1][0], dp[i-1][1] + prices[i])
+// dp[i][1] = max(dp[i-1][1], dp[i-2][0] - prices[i])
+// dp[0][0] = 0
+// dp[0][1] = -prices[0]
 function maxProfit(prices: number[]): number {
+    const len = prices.length + 1;
+    const dp: number[][] = new Array(len).fill(0).map(() => new Array(2).fill(0));
+    dp[1][0] = 0;
+    dp[1][1] = -prices[0];
+    for (let i = 2; i < len; i++) {
+        dp[i][0] = Math.max(dp[i - 1][0], dp[i - 1][1] + prices[i - 1]);
+        dp[i][1] = Math.max(dp[i - 1][1], dp[i - 2][0] - prices[i - 1]);
+    }
+    return dp[len - 1][0];
+};
+
+// 暴力解
+function maxProfit1(prices: number[]): number {
     let max = 0;
     //state 1：持有 2：未持有 3：冷冻期
     const operation = (index: number, state: number, cost: number) => {
