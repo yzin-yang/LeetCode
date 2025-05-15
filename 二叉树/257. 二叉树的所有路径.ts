65de503a26d4f685ece1218253e8a63c6a783734
yzin <734880084@qq.com>
2025-05-15 23:27:42 +0800

    Create 257. 二叉树的所有路径.ts

---
+++ 二叉树/257. 二叉树的所有路径.ts
@@ -0,0 +1,29 @@
+class TreeNode {
+    val: number
+    left: TreeNode | null
+    right: TreeNode | null
+    constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+        this.val = (val === undefined ? 0 : val)
+        this.left = (left === undefined ? null : left)
+        this.right = (right === undefined ? null : right)
+    }
+}
+
+function binaryTreePaths(root: TreeNode | null): string[] {
+    const res: string[] = [];
+    const recur = (node: TreeNode | null, path: number[]) => {
+        if (node === null) {
+            return;
+        }
+        path.push(node.val);
+        if (node.left === null && node.right === null) {
+            res.push(path.join('->'));
+        }
+        recur(node.left, path);
+        recur(node.right, path);
+        path.pop();
+        return;
+    }
+    recur(root, []);
+    return res;
+};
