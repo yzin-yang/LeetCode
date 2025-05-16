21a51cfa6716ece0175cad4f3d6bc37fc56b9bc8
yzin <734880084@qq.com>
2025-05-16 15:24:04 +0800

    Create 112. 路径总和.ts

---
+++ 二叉树/112. 路径总和.ts
@@ -0,0 +1,25 @@
+class TreeNode {
+    val: number
+    left: TreeNode | null
+    right: TreeNode | null
+    constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+        this.val = (val === undefined ? 0 : val)
+        this.left = (left === undefined ? null : left)
+        this.right = (right === undefined ? null : right)
+    }
+}
+
+function hasPathSum(root: TreeNode | null, targetSum: number): boolean {
+    if (root === null) {
+        return false;
+    }
+    if (root.left === null && root.right === null) {
+        if (targetSum - root.val === 0) {
+            return true;
+        }
+        return false;
+    }
+    const left = hasPathSum(root.left, targetSum - root.val);
+    const right = hasPathSum(root.right, targetSum - root.val);
+    return left || right;
+};
