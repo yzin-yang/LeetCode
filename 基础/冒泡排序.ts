6df8e157dc5f8f0293457b3f840b39568310b9e4
yzin <734880084@qq.com>
2025-06-08 02:37:21 +0800

    Create 冒泡排序.ts

---
+++ 基础/冒泡排序.ts
@@ -0,0 +1,14 @@
+function bubbleSort(nums: number[]) {
+    for (let i = 0; i < nums.length - 1; i++) {
+        for (let j = 0; j < nums.length - 1 - i; j++) {
+            if (nums[j] > nums[j + 1]) {
+                const temp = nums[j];
+                nums[j] = nums[j + 1];
+                nums[j + 1] = temp;
+            }
+        }
+    }
+    return nums;
+}
+
+console.log(bubbleSort([2, 3, 1]));
