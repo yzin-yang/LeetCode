c291bfd9e3d6dc38a357b74693a11d7f773158f2
yzin <734880084@qq.com>
2025-06-08 02:46:34 +0800

    Create 选择排序.ts

---
+++ 基础/选择排序.ts
@@ -0,0 +1,17 @@
+function selectionSort(nums: number[]) {
+    let minIndex = 0;
+    for (let i = 0; i < nums.length - 1; i++) {
+        minIndex = i;
+        for (let j = i + 1; j < nums.length; j++) {
+            if (nums[j] < nums[minIndex]) {
+                minIndex = j;
+            }
+        }
+        const temp = nums[i];
+        nums[i] = nums[minIndex];
+        nums[minIndex] = temp;
+    }
+    return nums;
+}
+
+console.log(selectionSort([2, 3, 1]));
